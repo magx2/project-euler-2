fc5fa52bb18fe76edce8e694c59f54391b9591e3
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 11:28:57 +0100

    Implemented FibonacciGenerator

--- project-euler-2/src/main/java/pl/grzeslowski/project_euler_2/FibonacciGenerator.java
+++ project-euler-2/src/main/java/pl/grzeslowski/project_euler_2/FibonacciGenerator.java
@@ -1,7 +1,37 @@
 package pl.grzeslowski.project_euler_2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class FibonacciGenerator {
+    private final Map<Integer, Long> cache = new HashMap<Integer, Long>();
+
+    public FibonacciGenerator() {
+        cache.put(1, 1L);
+        cache.put(2, 2L);
+    }
+
     public long getFibonacciTerm(final int index) {
-        return 0;
+        if (index <= 0) {
+            throw new IllegalArgumentException(
+                    "Term index cannot be lower o equals 0!");
+        }
+
+        if (cache.containsKey(index)) {
+            return cache.get(index);
+        } else {
+            long left=1;
+            long right=1;
+            
+            for (int i = 2; i <= index; i++) {
+                final long tmp = left + right;
+                left=right;
+                right = tmp;
+            }
+            
+            cache.put(index, right);
+            
+            return right;
+        }
     }
 }
